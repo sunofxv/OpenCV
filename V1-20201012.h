0c477d2caeffef4d25e7a3f063b78905667b82d6
muziY <44505038+sunofxv@users.noreply.github.com>
2020-11-17 23:13:28 +0800

    Add files via upload

---
+++ V1-20201012.h
@@ -0,0 +1,32 @@
+﻿
+// V1-20201012.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CV120201012App:
+// 有关此类的实现，请参阅 V1-20201012.cpp
+//
+
+class CV120201012App : public CWinApp
+{
+public:
+	CV120201012App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CV120201012App theApp;
---
+++ V1-20201012Dlg.h
@@ -0,0 +1,54 @@
+﻿
+// V1-20201012Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CV120201012Dlg 对话框
+class CV120201012Dlg : public CDialogEx
+{
+// 构造
+public:
+	CV120201012Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_V120201012_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+public:
+	void showImg();
+	void changeImg();
+	void yanMo();
+	void createNew();
+	void imgCopy();
+	void PixlShow();
+	void pixelOperator();
+	void trackingBar();
+	void keyboardDemo();
+	void colorMap();
+	void bitwiseDemo();
+	void channelDemo();
+	void pixel_statistic();
+	void drawDemo();
+	void randomDraw();
+	void mutiplyDraw();
+	void mouseDraw_Demo();
+};
+
+static void onTrack(int b, void*);
